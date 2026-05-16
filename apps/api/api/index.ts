d84c4087b65dd8880d577c4bfbf156bf5f88b620
CarlosEmerito <carlos09102007@gmail.com>
2026-05-16 03:58:38 +0200

    feat: design elegant and accessible 404 page (v0.1.0-alpha.12)

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -5,7 +5,7 @@ import { AppModule } from '../src/modules/app/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 
-const VERSION = '0.1.0-alpha.11';
+const VERSION = '0.1.0-alpha.12';
 
 let cachedExpressApp: any;
 
