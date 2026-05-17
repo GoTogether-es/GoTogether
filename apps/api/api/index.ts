74bb947126ef2b892c7e011e66327750d2aeb713
CarlosEmerito <carlos09102007@gmail.com>
2026-05-18 00:04:21 +0200

    chore: bump version to 0.1.0-alpha.14

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -5,7 +5,7 @@ import { AppModule } from '../src/modules/app/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 
-const VERSION = '0.1.0-alpha.13';
+const VERSION = '0.1.0-alpha.14';
 
 const allowedOrigins = [
   process.env.FRONTEND_URL,
